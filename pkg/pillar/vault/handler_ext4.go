rucoder/eve

Add mismatching PCR indexes to VaultStatus
Before this commit PCR indexes were present only in error text.
Add them to VaultStatus to simplify further analysis

Signed-off-by: Mikhail Malyshev <mike.malyshev@gmail.com>

pkg/pillar/vault/handler_ext4.go
@@ -424,6 +424,11 @@ func (h *Ext4Handler) getVaultStatus(vaultName string, vaultPath string,
 				status.Status = info.DataSecAtRestStatus_DATASEC_AT_REST_ENABLED
 			} else {
 				status.Status = info.DataSecAtRestStatus_DATASEC_AT_REST_ERROR
+				if status.PCRStatus == info.PCRStatus_PCR_ENABLED {
+					if pcrs, err := etpm.FindMismatchingPCRs(); err == nil {
+						status.MissmatchingPCRs = pcrs
+					}
+				}
 				status.SetErrorDescription(types.ErrorDescription{Error: "Vault key unavailable"})
 			}
 		}

bibin-paul-trustme/eve1

pkg/pillar/vault/handler_ext4.go
@@ -424,6 +424,11 @@ func (h *Ext4Handler) getVaultStatus(vaultName string, vaultPath string,
 				status.Status = info.DataSecAtRestStatus_DATASEC_AT_REST_ENABLED
 			} else {
 				status.Status = info.DataSecAtRestStatus_DATASEC_AT_REST_ERROR
+				if status.PCRStatus == info.PCRStatus_PCR_ENABLED {
+					if pcrs, err := etpm.FindMismatchingPCRs(); err == nil {
+						status.MismatchingPCRs = pcrs
+					}
+				}
 				status.SetErrorDescription(types.ErrorDescription{Error: "Vault key unavailable"})
 			}
 		}